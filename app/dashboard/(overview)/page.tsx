b8913d9cf69a5863a1dbd7bcdc173dd4f4e2cc18
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-10-27 02:00:32 +0500

    Done Chapter 10

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,17 +1,14 @@
-import { Card } from '@/app/ui/dashboard/cards'
+
 import RevenueChart from '@/app/ui/dashboard/revenue-chart'
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
 import { lusitana } from '@/app/ui/fonts'
-import { fetchCardData } from '@/app/lib/data'
 import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
-  CardSkeleton,
   CardsSkeleton
 } from '@/app/ui/skeletons'
 import React from 'react'
 import CardWrapper from '@/app/ui/dashboard/cards'
-// import { CardSkeleton } from '@/app/ui/skeletons'
 
 export default async function Page () {
   return (
